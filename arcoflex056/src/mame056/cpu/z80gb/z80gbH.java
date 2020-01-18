a70bfad63aefbe0c2a124633c35b8ffa1a5bf111
georgemoralis <giorgosmrls@gmail.com>
2020-01-18 10:08:28 +0200

    dummy z80gb cpu (executes with no instructions)

--- arcoflex056/src/mame056/cpu/z80gb/z80gbH.java
+++ arcoflex056/src/mame056/cpu/z80gb/z80gbH.java
@@ -3,40 +3,41 @@
  */
 package mame056.cpu.z80gb;
 
-import mame056.cpu.z80.*;
+import static mame056.memory.cpu_readmem16;
+import static mame056.memory.cpu_writemem16;
 
 public class z80gbH {
 
-    public static final int Z80_PC = 1;
-    public static final int Z80_SP = 2;
-    public static final int Z80_AF = 3;
-    public static final int Z80_BC = 4;
-    public static final int Z80_DE = 5;
-    public static final int Z80_HL = 6;
-    public static final int Z80_IX = 7;
-    public static final int Z80_IY = 8;
-    public static final int Z80_AF2 = 9;
-    public static final int Z80_BC2 = 10;
-    public static final int Z80_DE2 = 11;
-    public static final int Z80_HL2 = 12;
-    public static final int Z80_R = 13;
-    public static final int Z80_I = 14;
-    public static final int Z80_IM = 15;
-    public static final int Z80_IFF1 = 16;
-    public static final int Z80_IFF2 = 17;
-    public static final int Z80_HALT = 18;
-    public static final int Z80_NMI_STATE = 19;
-    public static final int Z80_IRQ_STATE = 20;
-    public static final int Z80_DC0 = 21;
-    public static final int Z80_DC1 = 22;
-    public static final int Z80_DC2 = 23;
-    public static final int Z80_DC3 = 24;
+    public static final int Z80GB_PC = 1;
+    public static final int Z80GB_SP = 2;
+    public static final int Z80GB_AF = 3;
+    public static final int Z80GB_BC = 4;
+    public static final int Z80GB_DE = 5;
+    public static final int Z80GB_HL = 6;
+    public static final int Z80GB_IRQ_STATE = 7;
 
-    public static final int Z80_TABLE_op = 0;
-    public static final int Z80_TABLE_cb = 1;
-    public static final int Z80_TABLE_ed = 2;
-    public static final int Z80_TABLE_xy = 3;
-    public static final int Z80_TABLE_xycb = 4;
-    public static final int Z80_TABLE_ex = 5;/* cycles counts for taken jr/jp/call and interrupt latency (rst opcodes) */
+    /**
+     * *************************************************************************
+     */
+    /* Memory functions                                                         */
+    /**
+     * *************************************************************************
+     */
+    public static int mem_ReadByte(int addr) {
+        return cpu_readmem16(addr) & 0xFF;
+    }
+
+    public static void mem_WriteByte(int addr, int value) {
+        cpu_writemem16(addr, value & 0xFF);
+    }
+
+    public static int mem_ReadWord(int addr) {
+        return (mem_ReadByte(addr) | (mem_ReadByte((addr + 1) & 0xffff) << 8)) & 0xFFFF;
+    }
+
+    public static void mem_WriteWord(int address, int data) {
+        mem_WriteByte(address, data & 0xFF);
+        mem_WriteByte((address + 1) & 0xffff, data >> 8);
+    }
 
 }
