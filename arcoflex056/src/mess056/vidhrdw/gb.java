43673896cb1c6ec5b0c7acb062f1cfc8913da066
chusogar@gmail.com <jagsanchez@jagsanchez10.indra.es>
2020-02-18 18:27:54 +0100

    gameboy video fixed!

--- arcoflex056/src/mess056/vidhrdw/gb.java
+++ arcoflex056/src/mess056/vidhrdw/gb.java
@@ -141,16 +141,184 @@ public class gb
                 public int xend;
 	};
         
-        //static int last_line = -1;
+        public static final int SC_Y = 0xff42;
+        public static final int SC_X = 0xff43;
+        public static final int LCDC_CONTROL = 0xff40;
+        public static final int WINDOW_DISPLAY_ENABLE = 5;
+        public static final int SPRITE_ENABLE = 1;
+        public static final int BACKGROUND_ENABLE = 0;
+        
+        public static final int W_Y = 0xff4a;
+        public static final int W_X = 0xff4b;
+        
+        //sprite flags
+        private static final int PALETTE_NUM = 4;
+        private static final int HORIZ_FLIP = 5;
+        private static final int VERT_FLIP = 6;
+        private static final int PRIORITY = 7;
+        private static final int SPRITE_HEIGHT = 2;
         
         public static void gb_refresh_scanline ()
 	{
-		mame_bitmap bitmap = Machine.scrbitmap;
-                
-                int SC_Y = 0xff42;
-                int SC_X = 0xff43;
-                int LCDC_CONTROL = 0xff40;
-                
+	
+            mame_bitmap bitmap = Machine.scrbitmap;
+            int currentScanLine = CURLINE();
+            
+            int lcdc = gb_ram.read(LCDC_CONTROL) & 0xff;
+            if (isSet(lcdc, BACKGROUND_ENABLE))
+                drawBackground(bitmap, currentScanLine);
+            if (isSet(lcdc, WINDOW_DISPLAY_ENABLE))
+                drawWindow(bitmap, currentScanLine);
+            if (isSet(lcdc, SPRITE_ENABLE))
+                drawSprites(bitmap, currentScanLine);
+        }
+        
+        /**
+        * draws the window onto the LCD display screen
+        * as specified by wX and wY registers
+        */
+       private static void drawWindow(mame_bitmap bitmap, int scanLine) {
+           int lcdc = gb_ram.read(LCDC_CONTROL)&0xff;
+           int wX = gb_ram.read(W_X)&0xff;
+           int wY = gb_ram.read(W_Y)&0xff;
+           int tileMapAddress = isSet(lcdc, 6) ? 0x9c00 : 0x9800;
+           int tileDataAddress = isSet(lcdc, 4) ? 0x8000 : 0x9000;
+           boolean signedIndex = !isSet(lcdc, 4);
+           int bgTileInfo;
+
+
+           if (wY > 143 || wX > 166 || wY > scanLine) {
+               return; //window not on screen or on this line
+           }
+           //draw tiles on current scanLine
+           int yOffset = ((scanLine - wY) / 8) * 32;
+           int tileLine = (scanLine - wY) % 8;
+
+           for (int xTile = 0; xTile <= 20; ++xTile) {
+               int tileIndex = gb_ram.read(tileMapAddress + yOffset + xTile)&0xff;
+               //if (gbcMode)
+               //    bgTileInfo = memory.readVram(tileMapAddress + yOffset + xTile, 1);
+               //else
+                   bgTileInfo = 0;
+
+               int tileAddress = signedIndex ? (tileIndex * 16) + tileDataAddress
+                                             : (Byte.toUnsignedInt((byte) tileIndex) * 16) + tileDataAddress;
+
+               int xCoord = (wX - 7) + (xTile * 8);
+               drawWindowTile(bitmap, tileAddress, tileLine, xCoord, scanLine, bgTileInfo);
+           }
+       }
+       
+     /**
+     * Draws one window tile line at specified position
+     * Window doesn't wrap
+     *
+     * @param tileAddress of the tile
+     * @param line of the tile to draw (0 - 7)
+     * @param xPos on screen to draw top left of line
+     * @param yPos on screen to draw top left of line
+     */
+    private static void drawWindowTile(mame_bitmap bitmap, int tileAddress, int line, int xPos, int yPos, int bgTileInfo) {
+        int paletteAddress = 0xff47;
+
+        int pixByteA = gb_ram.read(tileAddress + (2 * line))&0xff;
+        int pixByteB = gb_ram.read(tileAddress + (2 * line) + 1)&0xff;
+
+        /*if (gbcMode) {
+            //set correct GBC tile info
+            paletteAddress = bgTileInfo & 0x7;
+            int bankNum = isSet(bgTileInfo, 3) ? 1 : 0;
+            pixByteA = memory.readVram(tileAddress + (2 * line), bankNum);
+            pixByteB = memory.readVram(tileAddress + (2 * line) + 1, bankNum);
+        }*/
+
+        //draw each pixel in the line
+        for (int pixel = 0; pixel <= 7; ++pixel) {
+            int colorNum = getPixelColorNum(pixByteA, pixByteB, pixel);
+            int xCoord = (xPos + pixel);
+            if (xCoord < 160 && yPos < 144 && xCoord >= 0 && yPos >= 0) {
+                //TODO is this bg palette?
+                plot_pixel.handler(bitmap, xCoord, yPos, colorNum);
+            }
+        }
+    }
+        
+        private static void drawSprites(mame_bitmap bitmap, int scanline) {
+            int lcdc = gb_ram.read(LCDC_CONTROL) & 0xff;
+            int height = isSet(lcdc, SPRITE_HEIGHT) ? 16 : 8;
+
+            //TODO for GBC mainly the same
+            // VRAM Bank #, different palette number
+
+            for (int i = 0; i < 40; ++i){
+                int offset = (39 - i) * 4;
+                int y = gb_ram.read(0xfe00 + offset)&0xff;
+                int x = gb_ram.read(0xfe00 + offset + 1)&0xff;
+                int tileNum = gb_ram.read(0xfe00 + offset + 2)&0xff;
+                int flags = gb_ram.read(0xfe00 + offset + 3)&0xff;
+                if (height == 16) {
+                    tileNum &= 0xfe;
+                }
+                if ((scanline >= (y - 16)) && ((y - 17) + height >= scanline)) {
+                    int address = (tileNum * 16) + 0x8000;
+                    draw_sprite_line(bitmap, x - 8, y -16, address,
+                                    flags, height, scanline);
+                }
+            }
+        }
+
+
+    /**
+     * draws the sprite for current scanline at address
+     * to the screen at xPos, yPos (top left corner
+     * @param x of LCD screen to display top left
+     * @param y of LCD screen to display top left
+     * @param address of first byte of sprite data
+     * @param height of the sprite (8 or 16)
+     * @param flags associated with the sprite
+     *
+     * TODO scanline LIMIT
+     */
+    private static void draw_sprite_line(mame_bitmap bitmap, int x, int y, int address, int flags, int height, int scanline) {
+        boolean horizFlip = isSet(flags, HORIZ_FLIP);
+        boolean hasPriority = !isSet(flags, PRIORITY);
+        int paletteAddress = isSet(flags, PALETTE_NUM) ? 0xff49 : 0xff48;
+        int sprite_line = height - (scanline - y);
+
+        int offset;
+        if (!isSet(flags, VERT_FLIP)) {
+            offset = 2 * (scanline - y);
+        } else {
+            offset = 2 * (sprite_line - 1);
+        }
+        int pixDataA = gb_ram.read(address + offset)&0xff;
+        int pixDataB = gb_ram.read(address + offset + 1)&0xff;
+
+        /*if (gbcMode) {
+            paletteAddress = flags & 0x7;
+            int bankNum = isSet(flags, 3) ? 1 : 0;
+            pixDataA = memory.readVram(address + offset, bankNum);
+            pixDataB = memory.readVram(address + offset + 1, bankNum);
+        }*/
+
+        for (int pix = 0; pix < 8; ++pix) {
+            int col_index = horizFlip ? 7 - pix : pix;
+            int color_num = getPixelColorNum(pixDataA, pixDataB, col_index);
+            int color = color_num;
+            if ((x + pix < 160) && (x + pix >= 0) && color_num != 0) {
+                if (hasPriority) {
+                    plot_pixel.handler(bitmap, x + pix, scanline, color);
+                } else {
+                    //if (bufferToColor(x + pix, scanline) == getColor(0, 0xff47, false)) {
+                        plot_pixel.handler(bitmap, x + pix, scanline, color);
+                    //}
+                }
+            }
+        }
+    }
+        
+        public static void drawBackground(mame_bitmap bitmap, int scanLine){
+            
                 int lcdc = gb_ram.read(LCDC_CONTROL) & 0xFF;
                 int tileMapAddress = isSet(lcdc, 3) ? 0x9c00 : 0x9800;
                 int scY = gb_ram.read(SC_Y) & 0xFF;
@@ -168,7 +336,7 @@ public class gb
                 // TODO HORIZ/VERT FLIP/ PRIORITY
 
                 //draw tiles on current scanLine
-                int scanLine = CURLINE();
+                
                 int yOffset = (((scY + scanLine) / 8) % 32) * 32;
                 int tileLine = (scY + scanLine) % 8;
 
@@ -204,11 +372,7 @@ public class gb
         
         public static void drawTile(mame_bitmap bitmap, int tileAddress, int line, int xPos, int yPos, int pixStart, int pixEnd, int bgTileInfo) {
             int paletteAddress = 0xff47;
-            int W_Y = 0xff4a;
-            int W_X = 0xff4b;
-            int LCDC_CONTROL = 0xff40;
-            int WINDOW_DISPLAY_ENABLE = 5;
-    
+            
             int wX = (gb_ram.read(W_X) & 0xff) - 7;
             int wY = gb_ram.read(W_Y) & 0xff;
             boolean windowDrawn = isSet(gb_ram.read(LCDC_CONTROL)&0xff, WINDOW_DISPLAY_ENABLE);
